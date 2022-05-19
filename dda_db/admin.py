8b4360a470c43f1e5ad8bc3feed115940d6fc447
sueng jun lee <wod8181@naver.com>
2022-05-20 01:12:17 +0900

    first commit

---
+++ dda_db/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import dda_db
+# Register your models here.
+
+admin.site.register(dda_db)
---
+++ dda_db/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class dda_db(models.Model):
+    rackToint = models.IntegerField() # ID?
+    stationName = models.CharField(max_length= 32) # 대여소 이름
+    parkingBikeToCnt = models.IntegerField() # 대여소에 남아있는 자전거 수
+    shared = models.IntegerField() # 빌려간 수
+    stationLatitude = models.FloatField() # Lat
+    stationLongitude = models.FloatField() # Long
+    stationId = models.CharField(max_length= 32) 
+
+    class Meta():
+        db_table = 'DDA_DB'
